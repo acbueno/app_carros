6055e6b982b886c8775757e1faee5c645e74d915
Anderson Bueno <anderson.carlosb@gmail.com>
2018-10-04 20:14:53 -0300

    capitulos 3 webview

--- app/src/main/java/br/com/livroandroid/carros/fragments/SiteLivroFragment.java
+++ app/src/main/java/br/com/livroandroid/carros/fragments/SiteLivroFragment.java
@@ -1,11 +1,16 @@
 package br.com.livroandroid.carros.fragments;
 
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import br.com.livroandroid.carros.R;
 import livroandroid.lib.fragment.BaseFragment;
@@ -13,6 +18,10 @@ import livroandroid.lib.fragment.BaseFragment;
 
 public class SiteLivroFragment extends BaseFragment {
 
+    private static final String URL_SOBRE = "http://www.livroandroid.com.br/sobre.htm";
+    private WebView webview;
+    private ProgressBar progress;
+    private SwipeRefreshLayout swipeLayout;
 
     public SiteLivroFragment() {
         // Required empty public constructor
@@ -22,8 +31,50 @@ public class SiteLivroFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_carros, container, false);
+        View view = inflater.inflate(R.layout.fragment_site_livro, container, false);
+        webview = (WebView) view.findViewById(R.id.webview);
+        progress = (ProgressBar) view.findViewById(R.id.progress);
+        setWebViewClient(webview);
+        // Carrega a página
+        webview.loadUrl(URL_SOBRE);
+        swipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeToRefresh);
+        swipeLayout.setOnRefreshListener(onRefreshListener());
+
+        swipeLayout.setColorSchemeResources(
+                R.color.refresh_progress_1,
+                R.color.refresh_progress_2,
+                R.color.refresh_progress_3);
+
         return view;
+    }
+
+    private void setWebViewClient(WebView webview) {
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView webview, String url, Bitmap favicon) {
+                super.onPageStarted(webview, url, favicon);
+                // Liga o progress
+                progress.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView webview, String url) {
+                // Desliga o progress
+                progress.setVisibility(View.INVISIBLE);
+                // Termina a animação do Swipe to Refresh
+
+            }
+
+        });
+    }
+
+    private SwipeRefreshLayout.OnRefreshListener onRefreshListener() {
+        return new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webview.reload(); // Atualiza a página
+            }
+        };
     }
 
 
